2010c5b1685583892278c073425630f45ec82f6a
Oleksii-Senchenko <106326629+Oleksii-Senchenko@users.noreply.github.com>
2023-08-12 14:48:19 +0300

    changeed state to useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,24 +5,22 @@ import Filter from './Filter/Filter';
 import { nanoid } from 'nanoid';
 
 export function App() {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  // const [contacts, setContacts] = useState();
+
+  const [contacts, setContacts] = useState(() => {
+    return (
+      JSON.parse(localStorage.getItem('contacts')) || [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+    );
+  });
 
   const [searchQuery, setSearchQuery] = useState('');
 
-  useEffect(() => {
-    const contact = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contact);
-
-    if (parsedContacts) {
-      setContacts(parsedContacts);
-    }
-  }, []);
-
+ 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
@@ -77,3 +75,4 @@ export function App() {
     </>
   );
 }
+
